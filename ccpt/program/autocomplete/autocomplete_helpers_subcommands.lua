80f57c5e5e01aea15f02b20a5e4eef39e8865748
PentagonLP <85071935+PentagonLP@users.noreply.github.com>
2025-06-01 14:28:57 +0200

    Make autocomplete package cache local
    Also format file a little bit

--- ccpt/program/autocomplete/autocomplete_helpers_subcommands.lua
+++ ccpt/program/autocomplete/autocomplete_helpers_subcommands.lua
@@ -5,31 +5,27 @@ local fileutils = _G.ccpt.loadmodule("/lib/fileutils")
 local autocomplete_helpers = {}
 
 -- Complete packageid (filter can be nil to display all, "installed" to only recommend installed packages or "not installed" to only recommend not installed packages)
-_G.ccpt.autocompletepackagecache = {}
-function autocomplete_helpers.completepackageid(curText,filterstate)
+local autocompletepackagecache = {}
+function autocomplete_helpers.completepackageid(curText, filterstate)
 	local result = {}
-	if curText=="" or curText==nil then
-		local packagedata = fileutils.readData(fs.combine(_G.ccpt.progdir,"packagedata"),false)
+	if (curText == "") or (curText == nil) then
+		local packagedata = fileutils.readData(fs.combine(_G.ccpt.progdir, "packagedata"), false)
 		if not packagedata then
 			return {}
 		end
-		_G.ccpt.autocompletepackagecache = packagedata
+		autocompletepackagecache = packagedata
 	end
     local installedversion
-	if not (filterstate==nil) then
-		installedversion = fileutils.readData(fs.combine(_G.ccpt.progdir,"installedpackages"),true)
+	if not (filterstate == nil) then
+		installedversion = fileutils.readData(fs.combine(_G.ccpt.progdir, "installedpackages"), true)
 	end
-	for i,v in pairs(_G.ccpt.autocompletepackagecache) do
-		if filterstate=="installed" then
-			if not (installedversion[i]==nil) then
-				result = autocomplete_common.addtoresultifitfits(i,curText,result)
-			end
-		elseif filterstate=="not installed" then
-			if installedversion[i]==nil then
-				result = autocomplete_common.addtoresultifitfits(i,curText,result)
-			end
+	for i, _ in pairs(autocompletepackagecache) do
+		if filterstate == "installed" and not (installedversion[i] == nil) then
+			result = autocomplete_common.addtoresultifitfits(i, curText, result)
+		elseif filterstate == "not installed" and installedversion[i] == nil then
+			result = autocomplete_common.addtoresultifitfits(i, curText, result)
 		else
-			result = autocomplete_common.addtoresultifitfits(i,curText,result)
+			result = autocomplete_common.addtoresultifitfits(i, curText, result)
 		end
 	end
 	return result
@@ -38,9 +34,9 @@ end
 -- Complete packageid, but only for custom packages, which is much simpler
 function autocomplete_helpers.completecustompackageid(curText)
 	local result = {}
-	local custompackages = fileutils.readData(fs.combine(_G.ccpt.progdir,"custompackages"),true)
-	for i,v in pairs(custompackages) do
-		result = autocomplete_common.addtoresultifitfits(i,curText,result)
+	local custompackages = fileutils.readData(fs.combine(_G.ccpt.progdir, "custompackages"), true)
+	for i, _ in pairs(custompackages) do
+		result = autocomplete_common.addtoresultifitfits(i, curText, result)
 	end
 	return result
 end
